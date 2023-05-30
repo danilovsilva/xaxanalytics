758fd8e44e7a296566f487ff5ab6116799e266b2
Joao Xavier <jpxaxa@gmail.com>
2023-05-30 16:44:56 -0300

    Add method to validate tags

--- main.py
+++ main.py
@@ -17,23 +17,24 @@ for arquivo in arquivos:
     a.main()
 
     # Deletar o arquivo
-    os.remove(caminho_arquivo)
+    # os.remove(caminho_arquivo)
 
 # Caminho do arquivo JSON a ser enviado
-# caminho_arquivo = "output/"
-# arquivos = os.listdir(caminho_arquivo)
+caminho_arquivo = "output/"
+arquivos = os.listdir(caminho_arquivo)
 
-# for arquivo in arquivos:
-#     # Abrir o arquivo JSON
-#     with open(caminho_arquivo + arquivo, "rb") as json:
-#         # Configurar o cabeçalho do request com o tipo de conteúdo
-#         headers = {'Content-Type': 'application/json'}
+for arquivo in arquivos:
+    # Abrir o arquivo JSON
+    with open(caminho_arquivo + arquivo, "rb") as json:
+        # Configurar o cabeçalho do request com o tipo de conteúdo
+        headers = {'Content-Type': 'application/json'}
 
-#         # Enviar o POST request com o arquivo JSON
-#         response = requests.post('https://xaxanalytics.fly.dev/api/matches', headers=headers, data=json)
+        # Enviar o POST request com o arquivo JSON
+        response = requests.post('https://xaxanalytics.fly.dev/api/matches', headers=headers, data=json)
 
-#     # Verificar o status da resposta
-#     if response.status_code == 200:
-#         print("Arquivo JSON enviado com sucesso.")
-#     else:
-#         print("Erro ao enviar o arquivo JSON:", response.text)
+    # Verificar o status da resposta
+    if response.status_code == 200:
+        print("Arquivo JSON enviado com sucesso.")
+        # os.remove(caminho_arquivo + arquivo)
+    else:
+        print("Erro ao enviar o arquivo JSON:", response.text)
